cff4e2914f61b2851fab612ae1d3e45976e292cf
jordansu <jdsu1996@gmail.com>
2020-09-07 10:28:13 +0800

    Updates

---
+++ precache-manifest.b834b183605dd6855be725e119b28b93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53486492eff9dc7484b93fc1b8cf4202",
+    "url": "/Testing/index.html"
+  },
+  {
+    "revision": "89077ee3e451da2e67bc",
+    "url": "/Testing/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/Testing/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Testing/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89077ee3e451da2e67bc",
+    "url": "/Testing/static/js/main.23d58871.chunk.js"
+  },
+  {
+    "revision": "2a162ca1dca606836ab6",
+    "url": "/Testing/static/js/runtime-main.a38eddb7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Testing/static/media/logo.5d5d9eef.svg"
+  }
+]);
